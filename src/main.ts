eee51cee19f6a0cbcf8d28af9442251006253598
Alexander Shelyugov <62933903+AlexanderShelyugov@users.noreply.github.com>
2024-05-17 01:21:12 +0200

    Connect Tailwind and PrimeVue

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,13 @@ import { createApp } from 'vue'
 import './style.css'
 import 'primevue/resources/themes/aura-light-green/theme.css'
 import App from './App.vue'
-import PrimeVue from 'primevue/config';
+import PrimeVue from 'primevue/config'
+// @ts-ignore
+import Lara from './presets/lara'
 
 createApp(App)
-    .use(PrimeVue)
+    .use(PrimeVue, {
+        unstyled: true,
+        pt: Lara
+    })
     .mount('#app')
